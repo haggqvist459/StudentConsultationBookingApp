f7bb05191d475e4c73a64375c3f6f40503161ba6
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2020-08-30 19:37:30 +1000

    added a headline and some text

--- src/js/components/signIn.js
+++ src/js/components/signIn.js
@@ -4,6 +4,7 @@ import { routingConstants, firebase, AuthContext } from '../utils';
 import { Redirect } from 'react-router';
 import { Grid, Typography, Button } from '@material-ui/core';
 import { Header } from '../components';
+import '../../sass/components/signIn.scss';
 
 
 const provider = new firebase.auth.GoogleAuthProvider();
@@ -18,9 +19,9 @@ const SignIn = function ({ history }) {
                     hd: "ait.edu.au"
                 });
                 firebase.auth().signInWithPopup(provider)
-                .then(() => {
-                    history.push(routingConstants.HOME); 
-                })
+                    .then(() => {
+                        history.push(routingConstants.HOME);
+                    })
             } catch (error) {
                 console.log(error);
             }
@@ -38,9 +39,17 @@ const SignIn = function ({ history }) {
             <Grid className='header'>
                 <Header />
             </Grid>
-            <Grid className='content'>
+            <Grid className='content' justify="center" alignItems="center">
                 {/* A sign in button here */}
-                <Button onClick={handleLoginSydney}>Sign In </Button>
+                <Grid container item xs={12} sm={12} md={12} lg={12} justify={"space-around"} alignItems={"center"}>
+                    <Grid container direction="column" justify={"center"} alignItems={"center"}>
+                        <Typography variant="h3" style={{textAlign: "center"}}> Student Consultation </Typography>
+                        <br />
+                        <Typography style={{textAlign: "center"}}> Sign in to access the calendar and request an appointment with a teacher.</Typography>
+                        <br />
+                        <Button variant="outlined" onClick={handleLoginSydney}>Sign In</Button>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     )
