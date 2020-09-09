c51186f22ba10927ceb6693c0fbddb1caba1e4f3
Tim Häggqvist Luotomäki <tim.haggqvist@gmail.com>
2020-09-09 18:04:02 +1000

    work from branch 2 included as well

--- src/js/pages/profile.js
+++ src/js/pages/profile.js
@@ -1,36 +1,60 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useContext } from 'react';
 import { withRouter } from 'react-router';
 import { Grid, Typography } from '@material-ui/core';
 import { Header } from '../components';
+import { adminConstants, roleConstants, AuthContext } from '../utils';
+import AdminProfile from '../components/profileAdmin';
+import TeacherProfile from '../components/profileTeacher';
+import StudentProfile from '../components/profileStudent';
 
 
 const Profile = function ({ history }) {
 
-    //click back to home page
-    const handleClick = useCallback(async event => {
-        event.preventDefault();
-    
-        try {
-          history.push('/home'); // switch /home to the constant
-        } catch (error) {
-          alert(error);
-        }
-    
-      }, [history]);
+  const { currentUserRole } = useContext(AuthContext);
+  const role = currentUserRole;
+  console.log("role", role);
+  //click back to home page
+  const handleClick = useCallback(async event => {
+    event.preventDefault();
 
-    return (
-        <Grid>
-            <Grid className='header'><Header /></Grid>
+    try {
+      history.push('/home'); // switch /home to the constant
+    } catch (error) {
+      alert(error);
+    }
 
-            <Grid className='content'>
+  }, [history]);
 
-                <Typography>profile page</Typography>
-                    
+  //fetch all the data from the database here and then pass it to the components as props.
 
-            </Grid>
+  return (
+    <Grid>
+      <Grid className='header'>
+        <Header />
+      </Grid>
 
+      <Grid container className='content' justify="center" alignItems="center">
+        <Grid container item xs={12} sm={12} md={12} lg={12} justify={"space-around"} alignItems={"center"}>
+          <Typography>profile page</Typography>
+          {{
+            student: (
+              <StudentProfile />
+            ),
+            admin: (
+              <AdminProfile />
+            ),
+            teacher: (
+              <TeacherProfile />
+            ),
+            default: (
+              <>Error</>
+            )
+          }[role]}
         </Grid>
-    )
+      </Grid>
+
+    </Grid>
+  )
 
 }
 
--- src/js/utils/authProvider.js
+++ src/js/utils/authProvider.js
@@ -1,24 +1,53 @@
 import React, { useEffect, useState, createContext } from 'react';
 import { firebase } from './fbConfig';
 import { Grid, CircularProgress } from '@material-ui/core'
+import { roleConstants, adminConstants } from './constants';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [currentUser, setCurrentUser] = useState();
+    const [currentUserRole, setCurrentUserRole] = useState("");
     const [pending, setPending] = useState(true);
 
-    useEffect(() => {       
+    const assignRole = (email) => {
+        localStorage.clear();
+
+        if (email.match(/^\d/)) {
+            // is the user a student
+            console.log("User is a student");
+            setCurrentUserRole(roleConstants.STUDENT);
+
+        }
+        else if (email === adminConstants.ADMIN1) {
+            // is the user an admin
+            console.log("User is an admin");
+            console.log(adminConstants.ADMIN1);
+            setCurrentUserRole(roleConstants.ADMIN);
+        }
+        else {
+            // the user is a teacher
+            console.log("User is a teacher");
+            setCurrentUserRole(roleConstants.TEACHER);
+        }
+    }
+
+    useEffect(() => {
         firebase.auth().onAuthStateChanged((user) => {
             setCurrentUser(user)
-            setPending(false)
-        }); 
-    }, []);
+            if (user) {
+                assignRole(user.email)
+                console.log("inside useEffect", currentUserRole)
+                setPending(false)
+            }
+
+        });
+    }, [currentUserRole]);
 
     if (pending) {
 
         return (
-            <Grid container item xs={12} justify="center" align="center" styles={{margin: "auto", marginTop: "auto"}}>
+            <Grid container item xs={12} justify="center" align="center" styles={{ margin: "auto", marginTop: "auto" }}>
                 <CircularProgress color="primary" />
             </Grid>
         )
@@ -26,7 +55,7 @@ export const AuthProvider = ({ children }) => {
     }
 
     return (
-        <AuthContext.Provider value={{ currentUser }}>
+        <AuthContext.Provider value={{ currentUser, currentUserRole }}>
             {children}
         </AuthContext.Provider>
     )
